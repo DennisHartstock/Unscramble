b655941dd9d7795f6ade37b8bfa2ec8242358efa
DennsHartstock <dennis.hartstock@web.de>
2023-08-10 07:55:58 +0200

    the lifecycle of a view model

--- app/src/main/java/com/example/android/unscramble/ui/game/GameViewModel.kt
+++ app/src/main/java/com/example/android/unscramble/ui/game/GameViewModel.kt
@@ -1,8 +1,13 @@
 package com.example.android.unscramble.ui.game
 
+import android.util.Log
 import androidx.lifecycle.ViewModel
 
 class GameViewModel : ViewModel() {
+
+    init {
+        Log.d("GameFragment", "GameViewModel created!")
+    }
 
     private var _score = 0
     val score: Int
@@ -15,5 +20,10 @@ class GameViewModel : ViewModel() {
     private var _currentScrambledWord = "test"
     val currentScrambledWord: String
         get() = _currentScrambledWord
+
+    override fun onCleared() {
+        super.onCleared()
+        Log.d("GameFragment", "GameViewModel destroyed!")
+    }
 
 }
